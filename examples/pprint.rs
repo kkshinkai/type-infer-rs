c77750f46a373c1640916056d83df0eb31dcd7de
Kk Shinkai <kkshinkai@kkshinkai.moe>
2022-07-18 17:11:13 +0800

    Format

--- examples/pprint.rs
+++ examples/pprint.rs
@@ -12,7 +12,10 @@ use type_infer_rs::expr::{expr::{Expr, Ident}, lit::Lit};
 fn main() {
     let expr = Expr::mk_let(
         Ident::new("id".to_string()),
-        Expr::mk_abs(Ident::new("x".to_string()), Expr::mk_var(Ident::new("x".to_string()))),
+        Expr::mk_abs(
+            Ident::new("x".to_string()),
+            Expr::mk_var(Ident::new("x".to_string())),
+        ),
         Expr::mk_app(
             Expr::mk_var(Ident::new("id".to_string())),
             Expr::mk_lit(Lit::Int(42)),
